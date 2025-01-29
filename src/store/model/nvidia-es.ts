c129c7791ea5337b21312459f632cca0efca186d
Arturo Gutiérrez <arturo.gutierrez@gmail.com>
2025-01-29 08:41:12 +0100

    feat: nvidia-es 5090

--- src/store/model/nvidia-es.ts
+++ src/store/model/nvidia-es.ts
@@ -55,6 +55,12 @@ export const NvidiaES: Store = {
       series: '3090',
       url: 'https://shop.nvidia.com/es-es/geforce/store/gpu/?page=1&limit=9&locale=es-es&category=GPU&gpu=RTX%203090&manufacturer=NVIDIA',
     },
+    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '5090',
+      url: 'https://marketplace.nvidia.com/es-es/consumer/graphics-cards/?locale=es-es&page=1&limit=9&gpu=RTX%205090&manufacturer=NVIDIA',
+    },
   ],
   name: 'nvidia-es',
 };
